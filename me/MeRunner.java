c0835b97d6470162940fa80f320b3c88f05abc1c
matthewbbrumley <matt@syntropy.io>
2016-02-11 16:04:38 -0500

    First Commit

---
+++ me/MeRunner.java
@@ -0,0 +1,18 @@
+public class MeRunner {
+    
+    //Variable Declaration
+    
+    //Method Declaration
+    //main is the function that runs first and starts the lifecycle
+    public static void main(String[] args) {
+        
+        // Allocate and Initialize your Person object
+        
+        
+        // Set the properties of your person object
+        
+        
+        // Output the Description
+    }
+    
+}
